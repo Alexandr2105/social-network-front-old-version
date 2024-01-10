8e3de1d2ed1aab91d891abc90847eaeedbd255e0
Alexandr2105 <5030553@gmail.com>
2024-01-10 11:35:13 +0300

    add Login.jsx

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SidebarContainer from "./components/Sidebar/SidebarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
 
 const App = () => {
     return (
@@ -30,6 +31,7 @@ const App = () => {
                 <Route path="/setting" render={() => <Settings/>}/>
                 <Route path="/friends" render={() => <FriendsContainer/>}/>
                 <Route path="/users" render={() => <UsersContainer/>}/>
+                <Route path="/login" render={() => <Login/>}/>
             </div>
         </div>);
 }
