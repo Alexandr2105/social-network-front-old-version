dc774208be13438212e608aabb799c333eec35ba
Alexandr2105 <5030553@gmail.com>
2024-01-23 16:37:16 +0300

    fix User.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -12,48 +12,49 @@ const Users = (props) => {
 
     return (
         <>
-        {props.isFetching ? <Preloader/> : null}
-            <div>
-                <div className={mod.pages}>
-                    {
-                        arrayPages.map(p => (
-                            <p key={p} className={props.currentPage === p ? mod.currentPage : ""}
-                               onClick={() => props.activePage(p)}>{p}</p>))
-                    }
-                </div>
-
+            {props.isFetching ? <Preloader/> :
                 <div>
-                    {props.users.map(u => (
-                            <div key={u.id} className={mod.user}>
-                                <div className={mod.avatarAndButton}>
-                                    <NavLink to={`/profile/${u.id}`}>
-                                        <img
-                                            src={u.profile === null || u.profile.avatar === null ? avatar : u.profile.avatar}
-                                            alt="avatar"/>
-                                    </NavLink>
-                                    <button
-                                        disabled={props.isClickButton.includes(u.id)}
-                                        onClick={() =>
-                                            props.followOrUnfollowShowStatus(u.id, u.follow)
-                                        }>
-                                        {u.follow ? "Follow" : "Unfollow"}
-                                    </button>
-                                </div>
-                                <div className={mod.informationAboutUser}>
-                                    <div className={mod.nameAndStatus}>
-                                        <p>{u.fullName}</p>
-                                        <p>{u.profile?.status}</p>
+                    <div className={mod.pages}>
+                        {
+                            arrayPages.map(p => (
+                                <p key={p} className={props.currentPage === p ? mod.currentPage : ""}
+                                   onClick={() => props.activePage(p)}>{p}</p>))
+                        }
+                    </div>
+
+                    <div>
+                        {props.users.map(u => (
+                                <div key={u.id} className={mod.user}>
+                                    <div className={mod.avatarAndButton}>
+                                        <NavLink to={`/profile/${u.id}`}>
+                                            <img
+                                                src={u.profile === null || u.profile.avatar === null ? avatar : u.profile.avatar}
+                                                alt="avatar"/>
+                                        </NavLink>
+                                        <button
+                                            disabled={props.isClickButton.includes(u.id)}
+                                            onClick={() =>
+                                                props.followOrUnfollowShowStatus(u.id, u.follow)
+                                            }>
+                                            {u.follow ? "Follow" : "Unfollow"}
+                                        </button>
                                     </div>
-                                    <div className={mod.location}>
-                                        <p>{u.profile?.country}</p>
-                                        <p>{u.profile?.city}</p>
+                                    <div className={mod.informationAboutUser}>
+                                        <div className={mod.nameAndStatus}>
+                                            <p>{u.fullName}</p>
+                                            <p>{u.profile?.status}</p>
+                                        </div>
+                                        <div className={mod.location}>
+                                            <p>{u.profile?.country}</p>
+                                            <p>{u.profile?.city}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    )}
+                            )
+                        )}
+                    </div>
                 </div>
-            </div>
+            }
         </>
     )
 }
