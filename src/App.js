a68aa1241bff3d26f52ca446422d6bd7c325599e
Alexandr2105 <5030553@gmail.com>
2024-02-29 13:05:59 +0300

    add lazy load and suspense for App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
-import React from "react";
+import React, {Suspense, lazy} from "react";
 import NavBar from "./components/Navbar/Navbar";
 import {Route, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import Musics from "./components/Musics/Musics";
-import MessagesContainer from "./components/Dialogs/MessagesContainer";
-import FriendsContainer from "./components/Friends/FriendsContainer";
 import SidebarContainer from "./components/Sidebar/SidebarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginContainer from "./components/Login/LoginContainer";
 import {compose} from "redux";
 import {connect} from "react-redux";
 import {appInitializing} from "./redux/appReducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import {getAppInitialized} from "./redux/selectors/app.Selector";
 import {getAuthToken} from "./redux/selectors/headerSelectors";
+
+const MessagesContainer = lazy(() => import("./components/Dialogs/MessagesContainer"))
+const FriendsContainer = lazy(() => import("./components/Friends/FriendsContainer"))
+const ProfileContainer = lazy(() => import("./components/Profile/ProfileContainer"))
+const LoginContainer = lazy(() => import("./components/Login/LoginContainer"))
 
 class App extends React.Component {
     componentDidMount() {
@@ -36,24 +37,34 @@ class App extends React.Component {
                     <SidebarContainer/>
                 </div>
                 <div className="nav-links">
-                    <Route path="/profile/:userId?"
-                           render={() => <ProfileContainer/>}/>
-                    <Route exact path="/message"
-                           render={() => <MessagesContainer/>}/>
+                    <Suspense fallback={Preloader}>
+                        <Route path="/profile/:userId?"
+                               render={() => <ProfileContainer/>}/>
+                        <Route exact path="/message"
+                               render={() => <MessagesContainer/>}/>
+                        <Route path="/login" render={() => <LoginContainer/>}/>
+                        <Route path="/friends" render={() => <FriendsContainer/>}/>
+                    </Suspense>
+
+
                     <Route path="/news" render={() => <News/>}/>
                     <Route path="/music" render={() => <Musics/>}/>
                     <Route path="/setting" render={() => <Settings/>}/>
-                    <Route path="/friends" render={() => <FriendsContainer/>}/>
+
                     <Route path="/users" render={() => <UsersContainer/>}/>
-                    <Route path="/login" render={() => <LoginContainer/>}/>
+
                 </div>
-            </div>);
+            </div>
+        );
     }
 }
 
 const mapStateToProps = (state) => ({
     appInitialized: getAppInitialized(state),
-    accessToken: getAuthToken(state),
+    accessToken:
+        getAuthToken(state),
 })
 
-export default compose(connect(mapStateToProps, {appInitializing}), withRouter)(App);
+export default compose(connect(mapStateToProps, {
+    appInitializing
+}), withRouter)(App);
